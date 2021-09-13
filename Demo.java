6efb99e8d3a7f631175fa7c7bf37f911867b3430
sureshgopalmca <61956871+sureshgopalmca@users.noreply.github.com>
2021-09-14 00:11:28 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo{
 public static void main(String args[]){
-System.out.println("Welcome to Java Jenkins Github");
+  for(int i=0;i<=5;i++){
+    System.out.println("Welcome to Java Jenkins Github");
+  }
 }
 }
